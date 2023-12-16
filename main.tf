023d41396898cea90e91d6775d68775637c0b777
HARDIK SINGHAL <singhal.hardik@mahindra.com>
2023-12-16 10:17:11 +0530

    modified bucket name

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 resource "aws_s3_bucket" "s3bucket" {
   count  = var.no_of_buckets
-  bucket = var.bucket_name[count.index]
+  bucket = var.name[count.index]
   lifecycle {
     ignore_changes = [tags]
   }
@@ -10,7 +10,7 @@ resource "aws_s3_bucket_versioning" "versioning_example" {
   count  = var.no_of_buckets
   bucket = aws_s3_bucket.s3bucket[count.index].id
   versioning_configuration {
-    status = var.versioning_enabled
+    status = "Enabled"
   }
 }
 
@@ -18,8 +18,8 @@ resource "aws_s3_bucket_public_access_block" "example" {
   count  = var.no_of_buckets
   bucket = aws_s3_bucket.s3bucket[count.index].id
 
-  block_public_acls       = var.block_public_acls
-  block_public_policy     = var.block_public_acls
-  ignore_public_acls      = var.block_public_acls
-  restrict_public_buckets = var.block_public_acls
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
 }
--- variables.tf
+++ variables.tf
@@ -2,25 +2,15 @@ variable "no_of_buckets" {
   description = "No of buckets to be created."
   type        = number
 }
-variable "bucket_name" {
-  description = "The name of the bucket. If omitted, Terraform will assign a random, unique name. Must be lowercase and less than or equal to 63 characters in length. "
+variable "name" {
+  description = "The name of the bucket."
   type        = list(string)
 }
-
 variable "region" {
   description = "The region where the bucket is created"
-  type        = string
-}
-variable "versioning_enabled" {
-  description = "Versioning for the bucket is enabled if set true (Enabled/Disabled/Suspended)"
   type        = string
 }
 variable "role_arn" {
   description = " The ARN of the IAM role"
   type = string
-}
-variable "block_public_acls" {
-  description = "To block all bucket public access"
-  type = bool
-  default = true
 }
