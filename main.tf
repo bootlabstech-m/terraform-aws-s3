6e2751c99390d0fda6f0426ad868bef818fb37f4
Bhargava Naidu <bhargava@bootlabstech.com>
2025-07-07 21:50:59 +0530

    new provider and cli version 5.46.0 and updated config

--- main.tf
+++ main.tf
@@ -1,17 +1,20 @@
 resource "aws_s3_bucket" "s3bucket" {
   count  = var.no_of_buckets
   bucket = var.name[count.index]
+  force_destroy = var.force_destroy
+  object_lock_enabled = var.object_lock_enabled
+
   lifecycle {
     ignore_changes = [tags]
   }
-
 }
 
 resource "aws_s3_bucket_versioning" "versioning_example" {
   count  = var.no_of_buckets
   bucket = aws_s3_bucket.s3bucket[count.index].id
+
   versioning_configuration {
-    status = "Enabled"
+    status = var.enable_versioning ? "Enabled" : "Suspended"
   }
 }
 
@@ -19,20 +22,48 @@ resource "aws_s3_bucket_public_access_block" "example" {
   count  = var.no_of_buckets
   bucket = aws_s3_bucket.s3bucket[count.index].id
 
-  block_public_acls       = true
-  block_public_policy     = true
-  ignore_public_acls      = true
-  restrict_public_buckets = true
+  block_public_acls       = var.block_public_acls
+  block_public_policy     = var.block_public_policy
+  ignore_public_acls      = var.ignore_public_acls
+  restrict_public_buckets = var.restrict_public_buckets
 }
-resource "aws_s3_bucket_server_side_encryption_configuration" "encrption-s3" {
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "encryption_s3" {
   count  = var.no_of_buckets
   bucket = aws_s3_bucket.s3bucket[count.index].id
-
   rule {
     apply_server_side_encryption_by_default {
       kms_master_key_id = var.aws_kms_key
       sse_algorithm     = "aws:kms"
     }
+    bucket_key_enabled = var.bucket_key_enabled
   }
-  
+}
+
+resource "aws_s3_bucket_logging" "logging" {
+  count = var.enable_logging ? var.no_of_buckets : 0
+  bucket = aws_s3_bucket.s3bucket[count.index].id
+  target_bucket = var.logging_target_bucket
+  target_prefix = var.logging_target_prefix
+}
+
+resource "aws_s3_bucket_lifecycle_configuration" "lifecycle" {
+  count = var.enable_lifecycle_rules ? var.no_of_buckets : 0
+  bucket = aws_s3_bucket.s3bucket[count.index].id
+
+  rule {
+    id     = var.rule_id
+    status = var.rule_status
+    expiration {
+      days = var.expiration_days
+    }
+
+    noncurrent_version_expiration {
+      noncurrent_days = var.noncurrent_version_expiration_days
+    }
+
+    abort_incomplete_multipart_upload {
+      days_after_initiation = var.abort_multipart_days
+    }
+  }
 }
--- variables.tf
+++ variables.tf
@@ -1,21 +1,120 @@
 variable "no_of_buckets" {
-  description = "No of buckets to be created."
+  description = "Number of S3 buckets to create"
   type        = number
 }
+
 variable "name" {
-  description = "The name of the bucket."
+  description = "List of S3 bucket names"
   type        = list(string)
+}
+
+variable "force_destroy" {
+  description = "Force destroy buckets (deletes all objects including versions)"
+  type        = bool
+  default     = false
+}
+
+variable "object_lock_enabled" {
+  description = "Whether to enable S3 Object Lock (must be enabled at creation)"
+  type        = bool
+  default     = false
+}
+
+variable "enable_versioning" {
+  description = "Enable S3 versioning"
+  type        = bool
+  default     = true
+}
+
+variable "aws_kms_key" {
+  description = "KMS key ID or ARN to use for encryption"
+  type        = string
+}
+
+variable "bucket_key_enabled" {
+  description = "Whether to use bucket keys for SSE-KMS"
+  type        = bool
+  default     = true
+}
+
+# Public Access Block
+variable "block_public_acls" {
+  type    = bool
+  default = true
+}
+variable "block_public_policy" {
+  type    = bool
+  default = true
+}
+variable "ignore_public_acls" {
+  type    = bool
+  default = true
+}
+variable "restrict_public_buckets" {
+  type    = bool
+  default = true
+}
+
+# Logging
+variable "enable_logging" {
+  description = "Enable server access logging"
+  type        = bool
+  default     = false
+}
+
+variable "logging_target_bucket" {
+  description = "Bucket where logs will be stored"
+  type        = string
+  default     = ""
+}
+
+variable "logging_target_prefix" {
+  description = "Prefix for log files"
+  type        = string
+  default     = "logs/"
+}
+
+# Lifecycle Rules
+variable "enable_lifecycle_rules" {
+  description = "Enable lifecycle rule for objects"
+  type        = bool
+  default     = false
+}
+
+variable "expiration_days" {
+  description = "Days after which objects expire"
+  type        = number
+  default     = 365
+}
+
+variable "noncurrent_version_expiration_days" {
+  description = "Days to retain non-current versions"
+  type        = number
+  default     = 30
+}
+variable "rule_id" {
+  description = "Identifier for the lifecycle rule"
+  type        = string
+  default     = "log-cleanup"
+}
+
+variable "rule_status" {
+  description = "Status of the lifecycle rule. Acceptable values: Enabled or Disabled"
+  type        = string
+  default     = "Enabled"
+}
+
+variable "abort_multipart_days" {
+  description = "Days after which incomplete multipart uploads are aborted"
+  type        = number
+  default     = 7
 }
 variable "region" {
   description = "The region where the bucket is created"
   type        = string
 }
+
 variable "role_arn" {
   description = " The ARN of the IAM role"
   type = string
-}
-variable "aws_kms_key" {
-  description = "kms_key"
-  type = string
-  
 }
